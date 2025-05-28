5f4a79d9be8cae9e7e3175e12b0a47caa8c18614
Ojash Mishra <ojash.mishra@cognizant.com>
2025-05-28 11:48:58 +0530

    updating basic structure

---
+++ DBOperationsWithEFCore_App/Program.cs
@@ -0,0 +1,45 @@
+using DBOperationsWithEFCore_App.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+       
+
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+        );
+
+
+
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+
+    }
+}
